1ac26b9abd441ea97e3eabed8b6cb34754ec24ea
omid <omid.nazifi@gmail.com>
2019-03-19 18:41:31 +0100

    add Product class

---
+++ Product.java
@@ -0,0 +1,16 @@
+class Product {
+	
+	private String name = "Coca Cola";
+	
+	Product() {
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+}
